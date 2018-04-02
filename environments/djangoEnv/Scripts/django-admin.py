42c53e0c178474ba686f7847a2caff0f98a32a0a
Rebecca <reby.yin@gmail.com>
2018-04-01 20:06:52 -0700

    Initial commit

---
+++ environments/djangoEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rebecca\desktop\event_organizer\environments\djangoenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
